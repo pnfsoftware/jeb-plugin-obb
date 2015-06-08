1b38fb2e23f1a4cca6a803c48d75db437cce8dfb
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-08 13:13:10 -0700

    Move plugin information to external class

---
+++ src/com/pnf/PluginData.java
@@ -0,0 +1,7 @@
+package com.pnf;
+
+public class PluginData {
+	public static String ID = "obb_plugin";
+	public static String OBB_NAME = "obb_file";
+	public static String FAT_IMAGE_NAME = "obb_image";
+}
